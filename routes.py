909e45fd60372664b9f37238e85af452535988e4
Bel0rusochka <kulinkovich.02@gmail.com>
2024-11-09 18:23:04 +0100

    Fixes #2

--- routes.py
+++ routes.py
@@ -32,7 +32,6 @@ def registrate_routes(app, db):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             if session.get('user') is not None:
-                print(session['user'])
                 active_user = Users.get_user_or_404(session['user'])
                 if active_user.is_banned:
                     flash("You are banned", "error")
